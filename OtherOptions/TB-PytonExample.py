2fca7f8230fd89f9880001671d8cb9464169f51f
oldguardmd <oldguard@msn.com>
2022-09-27 14:52:23 -0400

    Restructured, not working

--- OtherOptions/TB-PytonExample.py
+++ OtherOptions/TB-PytonExample.py
@@ -13,6 +13,7 @@ or implied.
 """
 
 #import requests
+from logging.config import dictConfig
 import time
 import base64
 import json
@@ -23,6 +24,67 @@ import urllib.request
 from email.message import Message
 import ssl
 import argparse
+import csv
+
+
+class csvProcessing():
+    def _init_(self, headers: dict, args: dict, powerSupplyCSV, temperatureCSV ):
+        self.address = args.address
+        self.reportDirectory = args.reportDirectory
+        self.powerSupplyCSV = powerSupplyCSV
+        self.temperatureCSV = temperatureCSV
+        self.headers = headers
+        return
+    def supplyProcessing(self):
+        response = self.request(url="https://{0}/redfish/v1/Chassis/1/Power".format(self.address), headers=self.headers)
+        for supply in response.json()["PowerSupplies"]:
+            supplyItems = ["PowerOutputWatts","LineInputVoltage","Name","PowerInputWatts","LastPowerOutputWatts"]
+        for item in supplyItems:
+            self.powerSupplyCSV.write(str(supply[item])+",") #Cols key
+        self.powerSupplyCSV.write("\n")
+        return
+    def temperatureProcessing(self):
+        response = request(url="https://"+address+"/redfish/v1/Chassis/1/Thermal", headers=headers)
+        tempItems = ["TEMP_SENS_FRONT","DIMM_A1_TMP","DIMM_B1_TMP","DIMM_C1_TMP","DIMM_D1_TMP","DIMM_E1_TMP","DIMM_F1_TMP","DIMM_G1_TMP","DIMM_H1_TMP","P1_TEMP_SENS","PSU1_TEMP","PSU2_TEMP"]
+        for temp in response.json()["Temperatures"]:
+            tempFile.write(str(temp["ReadingCelsius"])+",") #Cols temp["Name"]
+        return
+    def request(
+        url: str,
+        headers: dict = None,
+        method: str = "GET",
+        error_count: int = 0,
+    ) -> Response:
+        method = method.upper()
+        headers = headers or {}
+        params = params or {}
+        headers = {"Accept": "application/json", **headers}
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+        httprequest = urllib.request.Request(
+            url, headers=headers, method=method
+        )
+        try:
+            with urllib.request.urlopen(httprequest, context=ctx) as httpresponse:
+                response = Response(
+                    headers=httpresponse.headers,
+                    status=httpresponse.status,
+                    body=httpresponse.read().decode(
+                        httpresponse.headers.get_content_charset("utf-8")
+                    ),
+                )
+        except urllib.error.HTTPError as e:
+            response = Response(
+                body=str(e.reason),
+                headers=e.headers,
+                status=e.code,
+                error_count=error_count + 1,
+            )
+        if not (200 <= response.status < 300):
+            print("Don't have access to the API requested") 
+            exit()
+        return response
 
 class Response(typing.NamedTuple):
     body: str
@@ -35,88 +97,31 @@ class Response(typing.NamedTuple):
         except json.JSONDecodeError:
             output = ""
         return output
-def request(
-    url: str,
-    data: dict = None,
-    params: dict = None,
-    headers: dict = None,
-    method: str = "GET",
-    data_as_json: bool = True,
-    error_count: int = 0,
-) -> Response:
-    method = method.upper()
-    request_data = None
-    headers = headers or {}
-    data = data or {}
-    params = params or {}
-    headers = {"Accept": "application/json", **headers}
-    if method == "GET":
-        params = {**params, **data}
-        data = None
-    if params:
-        url += "?" + urllib.parse.urlencode(params, doseq=True, safe="/")
-    if data:
-        if data_as_json:
-            request_data = json.dumps(data).encode()
-            headers["Content-Type"] = "application/json; charset=UTF-8"
-        else:
-            request_data = urllib.parse.urlencode(data).encode()
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
-    httprequest = urllib.request.Request(
-        url, data=request_data, headers=headers, method=method
-    )
-    try:
-        with urllib.request.urlopen(httprequest, context=ctx) as httpresponse:
-            response = Response(
-                headers=httpresponse.headers,
-                status=httpresponse.status,
-                body=httpresponse.read().decode(
-                    httpresponse.headers.get_content_charset("utf-8")
-                ),
-            )
-    except urllib.error.HTTPError as e:
-        response = Response(
-            body=str(e.reason),
-            headers=e.headers,
-            status=e.code,
-            error_count=error_count + 1,
-        )
 
-    return response
 
 helpmsg = """
-    Help will eventually be here.
+    This will poll power and temperature readings from a Cisco Stand-alone C series server using the Redfish API.
 """
 argsParse = argparse.ArgumentParser(description=helpmsg)
 argsParse.add_argument('-a', '--address',         action='store', dest='address',         default='',      help="System to get API Data from")
 argsParse.add_argument('-u', '--user',            action='store', dest='username',        default='admin', help='User Name to access the API' )
 argsParse.add_argument('-p', '--password',        action='store', dest='password',        default='',      help="Password for API Access")
 argsParse.add_argument('-r', '--reportDirectory', action='store', dest="reportDirectory", default='./',    help="Location directory for files")
+argsParse.add_argument('-c', '--count', type=int  action='store', dest='counter',         default='1',     help="Number of times to repeat")
 args=argsParse.parse_args()
 address = "{}".format(args.address)
 username = "{}".format(args.username)
 password = "{}".format(args.password)
 
-while True:
-    tempFile = open("{0}{1}-temperature.csv".format(args.reportDirectory,address), "a")
-    supplyFile = open("{0}{1}-powersupply.csv".format(args.reportDirectory,address), "a")
-    headers = {'Authorization': 'Basic '+base64.b64encode((username+":"+password).encode('ascii')).decode("utf-8") }
-    response = request(url="https://"+address+"/redfish/v1/Chassis/1/Power", headers=headers, data={})
-    if not (200 <= response.status < 300):
-        print("Don't have access to the API requested") 
-    for supply in response.json()["PowerSupplies"]:
-        supplyItems = ["PowerOutputWatts","LineInputVoltage","Name","PowerInputWatts","LastPowerOutputWatts"]
-        for item in supplyItems:
-            supplyFile.write(str(supply[item])+",") #Cols key
-    supplyFile.write("\n")
-    response = request(url="https://"+address+"/redfish/v1/Chassis/1/Thermal", headers=headers, data={})
-    tempItems = ["TEMP_SENS_FRONT","DIMM_A1_TMP","DIMM_B1_TMP","DIMM_C1_TMP","DIMM_D1_TMP","DIMM_E1_TMP","DIMM_F1_TMP","DIMM_G1_TMP","DIMM_H1_TMP","P1_TEMP_SENS","PSU1_TEMP","PSU2_TEMP"]
-    for temp in response.json()["Temperatures"]:
-        tempFile.write(str(temp["ReadingCelsius"])+",") #Cols temp["Name"]
-    tempFile.write("\n")
-    tempFile.close()
-    supplyFile.close()
-    time.sleep(60)
-
+headers = {'Authorization': 'Basic '+base64.b64encode((username+":"+password).encode('ascii')).decode("utf-8") }
+supplyFile = open("{0}{1}-powersupply.csv".format(self.reportDirectory,self.address), "a")
+tempFile = open("{0}{1}-temperature.csv".format(self.reportDirectory,self.address), "a")
+csvObject = csvProcessing(headers=headers,args=args,powersupplyCSV=supplyFile,temperatureCSV=tempFile)
+while True:        
+    csvObject.supplyProcessing()
+    csvObject.temperatureProcessing()    
+    time.sleep(10)
+    exit()
+tempFile.write("\n")
+tempFile.close()
+supplyFile.close()
